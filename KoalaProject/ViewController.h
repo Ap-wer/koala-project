a6e7ff968392992029145abbf7365211c6f608e1
Ap-wer <wer_chen@163.com>
2016-06-18 20:15:33 +0800

    alpaca first commit

---
+++ KoalaProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KoalaProject
+//
+//  Created by wer_chen on 16/6/18.
+//  Copyright © 2016年 wer_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KoalaProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KoalaProject
+//
+//  Created by wer_chen on 16/6/18.
+//  Copyright © 2016年 wer_chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
